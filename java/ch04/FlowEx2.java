321357147886854ec95ea90ae9985efefdcf7507
Jinny <dms873@naver.com>
2021-11-21 19:57:49 +0900

    java practice

---
+++ java/ch04/FlowEx2.java
@@ -0,0 +1,33 @@
+package ch04;
+
+import java.util.Scanner;
+
+public class FlowEx2 {
+  public static void main(String[] args) {
+    int input;
+
+    System.out.printf("숫자를 하나 입력하세요.>");
+
+    Scanner scanner = new Scanner(System.in);
+    String tmp = scanner.nextLine(); // 화면을 통해 입력받은 내용을 tmp에 저장
+    input = Integer.parseInt(tmp); // 입력받은 문자열(tmp)을 숫자로 변환
+
+    if (input == 0) {
+      System.out.println("입력하신 숫자는 0입니다.");
+    }
+
+    if (input != 0) {
+      System.out.println("입력하신 숫자는 0이 아닙니다.");
+      System.out.printf("입력하신 숫자는 %d입니다.", input);
+    }
+  } // main의 끝
+}
+
+// TODO 3을 입력했을 때,
+// 숫자를 하나 입력하세요.>3
+// 입력하신 숫자는 0이 아닙니다.
+// 입력하신 숫자는 3입니다.
+
+// TODO 0을 입력했을 때,
+// 숫자를 하나 입력하세요.>0
+// 입력하신 숫자는 0입니다.
